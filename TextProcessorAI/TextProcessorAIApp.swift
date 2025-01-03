c5e86a7d234d1f0fe93134741057a98b5e169211
redlemonbe <jfbaudson@icloud.com>
2025-01-03 23:14:14 +0100

    Initial Commit

---
+++ TextProcessorAI/TextProcessorAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextProcessorAIApp.swift
+//  TextProcessorAI
+//
+//  Created by Jean-François Baudson on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TextProcessorAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TextProcessorAITests/TextProcessorAITests.swift
@@ -0,0 +1,17 @@
+//
+//  TextProcessorAITests.swift
+//  TextProcessorAITests
+//
+//  Created by Jean-François Baudson on 03/01/2025.
+//
+
+import Testing
+@testable import TextProcessorAI
+
+struct TextProcessorAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
